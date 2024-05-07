7528edd19954b07e060105bedf444e8b07be7ed0
Maximkalenkovich <peterson.maxim@mail.ru>
2024-05-07 20:47:01 +0300

    add lib fs for working with json file

--- server/app/server.js
+++ server/app/server.js
@@ -11,3 +11,20 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+const fs = require('fs');
+
+let data;
+
+fs.readFile('data.json', 'utf8', (err, jsonString) => {
+    if (err) {
+        console.log('Error reading file:', err);
+        return;
+    }
+    try {
+        data = JSON.parse(jsonString);
+        console.log('Data parsed successfully');
+    } catch (err) {
+        console.log('Error parsing JSON string:', err);
+    }
+});
